8d5dff595fecb9359f00ff7e0bbe858c22d9ba64
Amy-Jassal <amy-jassal@hotmail.co.uk>
2023-09-29 15:23:59 +0100

    Add code to count frequency

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,12 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
-
+import collections
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    mylist = []
+    for x in items:
+        mylist.append(str(x))
+    counts = collections.Counter(mylist)
+    frequencies = dict(counts)
     return frequencies
